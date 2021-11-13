397a3dff59b7055b36bf523c2f6e0bfe5ff2a088
Shak <shakfeizi@gmail.com>
2021-11-13 16:30:29 -0700

    Signup View initiated

--- SigninSignup_Exp/SigninSignup_Exp/ContentView.swift
+++ SigninSignup_Exp/SigninSignup_Exp/ContentView.swift
@@ -63,6 +63,43 @@ struct SigninView: View {
     }
 }
 
+struct SignupView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @EnvironmentObject private var viewModel: ViewModel
+    
+    var body: some View {
+        VStack {
+            Image("UserLogo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            VStack {
+                TextField("Enter an email...", text: $email)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                SecureField("Enter a password...", text: $password)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                Button {
+                    guard !email.isEmpty, !password.isEmpty else { return }
+                    viewModel.signin(email: email, password: password)
+                } label: {
+                    Text("Sign In")
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+            .padding()
+            Spacer()
+        }
+        .navigationTitle(Text("Sign In"))
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
