6203132a9f7faeaae6c764b8a4015d265b2b4224
Shak <shakfeizi@gmail.com>
2021-11-14 09:19:10 -0700

    Bug Fixed

--- SigninSignup_Exp/SigninSignup_Exp/FirebaseManager.swift
+++ SigninSignup_Exp/SigninSignup_Exp/FirebaseManager.swift
@@ -9,16 +9,16 @@ import Foundation
 import Firebase
 
 
-class FirebaseManager {
-    // SHAK: Properties
-    static let shared: FirebaseManager = FirebaseManager()
-    var auth = Auth.auth()
-    var possible = false
-    
-    // SHAK: Functions
-    func signIn(email: String, password: String) {
-        auth.signIn(withEmail: email, password: password) { [weak self] (result, error) in
-            guard result != nil, error == nil else { return }
-        }
-    }
-}
+//class FirebaseManager {
+//    // SHAK: Properties
+//    static let shared: FirebaseManager = FirebaseManager()
+//    var auth = Auth.auth()
+//    var possible = false
+//    
+//    // SHAK: Functions
+//    func signIn(email: String, password: String, completion: @escaping(Result<String, Error>) -> Void) {
+//        auth.signIn(withEmail: email, password: password) { [weak self] (result, error) in
+//            guard result != nil, error == nil else { return }
+//        }
+//    }
+//}
